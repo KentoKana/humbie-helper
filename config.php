4700d604e92366d45e87a13b87266e622acdae7c
Mark Martin <n01321102@humbermail.ca>
2019-03-16 18:58:48 -0400

    added a fix for the trailing slashes issue that causes to break the app

--- config.php
+++ config.php
@@ -1,17 +1,16 @@
 <?php
 $path = realpath(__DIR__);
- $altpath= "http://localhost/project-backstreet-boys-and-jenna";
-//$altpath= "http://localhost/";
-//Had to use this path instead - Kento.
-
-// uncomment if you are using port 8080
-// $altpath= "http://localhost:8080/project-backstreet-boys-and-jenna";
+// gets the full URL
+// reference: https://stackoverflow.com/questions/6768793/get-the-full-url-in-php
+// author: ax.
+// date accessed: 2019-03-16
+$altpath = sprintf("//%s%s",$_SERVER["HTTP_HOST"],$_SERVER["REQUEST_URI"]);
 
 // config files for the app
-define('BASE', "$altpath\\");
-define('CSS', "$altpath\public\stylesheets");
-define('IMG', "$altpath\assets");
-define('JS', "$altpath\public\js");
-define('VIEWS', "$path\\views");
-define('INC', "$path\inc");
-define('LIB', "$path\lib");
+define('BASE', "$altpath/");
+define('CSS', "$altpath/public/stylesheets");
+define('IMG', "$altpath/assets");
+define('JS', "$altpath/public/js");
+define('VIEWS', "$altpath/views");
+define('INC', "$path/inc");
+define('LIB', "$path/lib");
