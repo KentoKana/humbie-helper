7b4f223cd58e6b2337d42049855ba6a0c1ba175f
RyeRob <ryan.robinson5@gmail.com>
2019-03-15 22:33:22 -0400

    added styling to add, edit and delete motivational quotes

--- views/quotes/delete-quote.php
+++ views/quotes/delete-quote.php
@@ -1,17 +1,19 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'].'/bsb-humber-web-dev-assistant/header.php';
+require_once '../../header.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ?>
-<body>
-    <h2>Are you sure you want to delete this quote?</h2>
-    <form action="#" method="POST">
-        <fieldset>
-            <ul>
-               <li id="form__author_print"></li>
-               <li id="form__quote_print"></li>
-            </ul>
-            <input type="submit" name="deletequote" value="Delete Quote">
-        </fieldset>
-    </form>
-</body>
+<main class="container filler">
+  <h1 class="my-4">Are you sure you want to delete this quote?</h1>
+  <form action="#" method="POST">
+    <div class="form-group">
+      <ul>
+        <li name="print-quote"></li>
+        <li name="print-quote-author"></li>
+      </ul>
+    </div>
+    <div class="form-group">
+      <input type="submit" class="btn btn-primary" name="delete-quote" value="Delete Quote">
+    </div>
+  </form>
+</main>
--- views/quotes/edit-quote.php
+++ views/quotes/edit-quote.php
@@ -1,21 +1,22 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'].'/bsb-humber-web-dev-assistant/header.php';
+require_once '../../header.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ?>
-<body>
-    <h2>Edit Quote/Author</h2>
-    <form action="#" method="POST">
-        <fieldset>
-            <div>
-                <label for="author">Quote Author: </label>
-                <input type="text" name="author" />
-            </div>
-            <div>
-                <label for="content">Quote: </label>
-                <input type="text" name="content" /><br />
-            </div>
-            <input type="submit" name="editquote" value="Edit Quote">
-        </fieldset>
-    </form>
-</body>
+<main class="container filler">
+  <h2 class="my-4">Edit Quote</h2>
+  <form action="#" method="POST">
+    <div class="form-group">
+      <label for="form__username-field">Quote: </label>
+      <input type="text" class="form__username-field form-control" id="form__username-field" placeholder="An example quote.">
+    </div>
+    <div class="form-group">
+      <label for="form__username-field">Quote Author: </label>
+      <input type="text" class="form__password-field form-control" id="form__username-field" placeholder="The author of that quote.">
+    </div>
+    <div class="form-group">
+      <button type="submit" id="form__submit-button" class="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</main>
+<?php require_once '../../footer.php'; ?>
