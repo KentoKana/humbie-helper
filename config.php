5b43d56fa5c645852e24bb9654b80919680b6ed1
Mark Martin <n01321102@humbermail.ca>
2019-03-16 20:03:35 -0400

    refactored the config file

--- config.php
+++ config.php
@@ -4,13 +4,14 @@ $path = realpath(__DIR__);
 // reference: https://stackoverflow.com/questions/6768793/get-the-full-url-in-php
 // author: ax.
 // date accessed: 2019-03-16
-$altpath = sprintf("//%s%s",$_SERVER["HTTP_HOST"],$_SERVER["REQUEST_URI"]);
-
+$uri = explode("/", $_SERVER["REQUEST_URI"]);
+array_splice($uri, -3);
+$altpath = sprintf("//%s/%s",$_SERVER["HTTP_HOST"],$uri[1]);
 // config files for the app
 define('BASE', "$altpath/");
 define('CSS', "$altpath/public/stylesheets");
 define('IMG', "$altpath/assets");
 define('JS', "$altpath/public/js");
-define('VIEWS', "$altpath/views");
+define('VIEWS', "$path/views");
 define('INC', "$path/inc");
 define('LIB', "$path/lib");
