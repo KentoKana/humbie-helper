8ac1cc9a6f153c270b7942d5480d162b5a89b91f
RyeRob <ryan.robinson5@gmail.com>
2019-04-02 20:41:08 -0400

    refactored quotes, worked on announcements

---
+++ models/quote_db.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'Database.php';
+class Quote
+{
+    //List All Quotes
+    public function getAllQuotes($db)
+    {
+        $query = "SELECT * FROM motivational_quotes";
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $quotes = $statement->fetchAll(PDO::FETCH_OBJ);
+        return $quotes;
+    }
+    //Return Specified Quote
+    public function getQuoteById($id, $db){
+        $query = "SELECT * FROM motivational_quotes 
+                  WHERE id = :id";
+        $statement = $db->prepare($query);
+        $statement->bindParam(':id', $id);
+        $statement->execute();
+        return $statement->fetch(PDO::FETCH_OBJ);
+    }
+    //Create New Quote
+    public function addQuote($quote_author, $quote, $db)
+    {
+        $query = "INSERT INTO motivational_quotes (quote_author, quote) 
+                  VALUES (:quote_author, :quote) ";
+        $statement = $db->prepare($query);
+        $statement->bindParam(':quote_author', $quote_author);
+        $statement->bindParam(':quote', $quote);
+        $count = $statement->execute();
+        return $count;
+    }
+    //Update Quote
+    public function editQuote($id, $quote_author, $quote, $db)
+    {
+        $query = "UPDATE motivational_quotes 
+                  SET quote_author = :quote_author, quote = :quote
+                  WHERE id = :id";
+        $statement = $db->prepare($query);
+        $statement->bindParam(':id', $id);
+        $statement->bindParam(':quote_author', $quote_author);
+        $statement->bindParam(':quote', $quote);
+        $count = $statement->execute();
+        return $count;
+    }
+    //Delete Quote
+    public function deleteQuote($id, $db)
+    {
+        $query = "DELETE FROM motivational_quotes 
+                  WHERE id = :id";
+        $statement = $db->prepare($query);
+        $statement->bindParam(':id', $id);
+        $count = $statement->execute();
+        return $count;
+    }
+}
--- views/quotes/edit-quote.php
+++ views/quotes/edit-quote.php
@@ -1,24 +1,29 @@
 <?php
-require '../../config.php';
-include VIEWS.'/header.php';
-require_once CONTROLLERS.'/quote-controller.php';
+require_once '../../config.php';
+require_once VIEWS . '/header.php';
+require_once MODELS . '/quote_db.php' ;
+require_once CONTROLLERS . '/quote-controller.php' ;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ?>
 <main id="jg-main" class="m-4">
     <h2 class="text-center pt-3">Edit Quote</h2>
     <div class="form form-group text-center px-5 py-2">
-    <form action="#" method="POST">
-        <div >
-            <label for="quoteInput">Quote: </label>
-            <input type="text" class="jg_form__text" name="quoteEdit" value="<?php $quote; ?>">
-        </div>
+    <form action="" method="POST">
+        <!-- Author Edit Feild -->
         <div>
-            <label for="">Quote Author: </label>
-            <input type="text" class="jg_form__text" value="<?php $quote_author; ?>">
+            <label for="quote_author">Quote Author:</label>
+            <input type="text" name="quote_author" 
+            value="<?php echo $quotebyid->quote_author; ?>" />
         </div>
+        <!-- Quote Edit Feild -->
+        <div class="pt-2 pl-5 pr-5">
+            <label for="quote">Quote:</label>
+            <input type="text" name="quote" value="<?php echo $quotebyid->quote; ?>" />
+        </div>
+        <!-- Submit Edited Quote-->
         <div>
-            <button type="submit" class="jg-form__submit" name="editquote">Submit</button>
+            <button type="submit" class="jg-form__submit" name="updatequote">Submit</button>
         </div>
     </form>
     </div>
